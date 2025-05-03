decfca0584deba3d39208c9c91e703d6160f8fa2
shaay98 <nrichardson567@icloud.com>
2025-05-03 02:10:40 +0000

    final commit

--- src/components /WeatherApp.jsx
+++ src/components /WeatherApp.jsx
@@ -4,31 +4,37 @@ import { useState } from "react";
 export default function WeatherApp() {
     const [weatherData, setWeatherData] = useState({});
     const API_KEY = import.meta.env.VITE_API_KEY;
-    useEffect(() => {
-        async function getData() {
-            try {
-                const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=New Orleans`);
-                console.log(res)
-                const data = await res.json();
-                
-                console.log(data)
-             setWeatherData(data);
-             console.log(data)
-            } catch (error) {
-                console.error(error);
-            }
-        }
-            getData();
     
-    }, []);
-    if (!weatherData.location){
+    
+    useEffect(() => {
+        async function getData() {        
+        
+        
+        try {
+        const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=New Orleans`);
+
+        const data = await res.json();
+        setWeatherData(data);
+         console.log(data);
+            } catch (error) {
+            console.error(error);
+        }
+        } getData();
+        },[API_KEY]);
+   
+        if (!weatherData.location){
         return <p>Loading..</p>
-    } else {
-        return (
-     <p> 
-     <strong>Temp:</strong>
-        {weatherData.location.current.temp}
-     </p>
+   
+        } else {
+
+     return (
+        <div>
+         <h2>Weather in {weatherData.location.name}, {weatherData.location.region}</h2>
+         <p>
+         <strong>Temperature:</strong> 
+         {weatherData.current.temp_f}
+         </p>
+         </div>
 );
 }
 }
